6ec76a393e0cc329a018fd7e1c5a683b95286314
Omar Faruk <omarfaruk65142@gmail.com>
2024-11-06 23:36:21 +0600

    Built in type

---
+++ data-type/bulitInType.js
@@ -0,0 +1,17 @@
+var firstName;
+var lastName;
+var userId;
+var isActive;
+userId = 190605;
+firstName = "Md. Omar";
+lastName = "Faruk";
+isActive = true;
+var fullName = firstName.concat(lastName);
+console.log({ userId: userId, fullName: fullName, isActive: isActive });
+console.log(fullName.split(" "));
+console.log(fullName.split(''));
+console.log(fullName.toLowerCase());
+console.log(fullName.toUpperCase());
+function display() {
+    console.log("Hello Programmer");
+}
---
+++ data-type/bulitInType.ts
@@ -0,0 +1,20 @@
+let firstName: string
+let lastName: string
+let userId: number
+let isActive: boolean
+userId = 190605
+firstName = "Md. Omar"
+lastName = "Faruk"
+isActive = true
+let fullName = firstName.concat(lastName)
+console.log({ userId, fullName, isActive })
+console.log(fullName.split(" "))
+console.log(fullName.split(''))
+console.log(fullName.toLowerCase())
+console.log(fullName.toUpperCase())
+
+
+
+function display(): void {
+    console.log("Hello Programmer")
+}
